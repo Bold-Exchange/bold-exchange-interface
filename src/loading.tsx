d11b6a448ba330a68539eb51ded8fef3151a5a97
doctor <doctor192322@gmail.com>
2024-06-18 11:05:00 +0800

    feat: add loading page

---
+++ src/loading.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+export default function () {
+  useEffect(() => {
+    (function ($) {
+      "use strict";
+
+      $(document).ready(function () {
+        //preloader
+        $("#preloader")
+          .delay(3500)
+          .animate(
+            {
+              opacity: "0",
+            },
+            500,
+            function () {
+              $("#preloader").css("display", "none");
+            }
+          );
+
+        //preloader counter
+        $(".counterr").counterUp({
+          delay: 10,
+          time: 2900,
+        });
+      });
+    })(jQuery);
+  });
+  return (
+    <div className="loader_wrapper" id="preloader">
+      <div className="loader">
+        <div className="face">
+          <div className="circle"></div>
+        </div>
+        <div className="face">
+          <div className="circle sd"></div>
+        </div>
+        <div className="percent">
+          <span className="counterr">100</span>
+          <span className="per">%</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,25 +21,6 @@ export default function HomePage() {
           wow.init();
         }
 
-        //preloader
-        $("#preloader")
-          .delay(3500)
-          .animate(
-            {
-              opacity: "0",
-            },
-            500,
-            function () {
-              $("#preloader").css("display", "none");
-            }
-          );
-
-        //preloader counter
-        $(".counterr").counterUp({
-          delay: 10,
-          time: 2900,
-        });
-
         //hambarger menu icon toggle
         $("#toggleIcon").click(function () {
           $(this).toggleClass("active");
@@ -388,21 +369,6 @@ export default function HomePage() {
   });
   return (
     <>
-      <div className="loader_wrapper" id="preloader">
-        <div className="loader">
-          <div className="face">
-            <div className="circle"></div>
-          </div>
-          <div className="face">
-            <div className="circle sd"></div>
-          </div>
-          <div className="percent">
-            <span className="counterr">100</span>
-            <span className="per">%</span>
-          </div>
-        </div>
-      </div>
-
       <header className="home_nav_two">
         <nav className="navbar fixed-top navbar-expand-lg navbar_two">
           <div className="container">
