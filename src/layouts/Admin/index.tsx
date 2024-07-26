4023ce5a8c73ed98e89228f2ebfeb9eb0e8c1146
wb_wangzhe11 <wb_wangzhe11@meituan.com>
2024-07-26 14:28:39 +0800

    feat：remove

--- src/layouts/Admin/index.tsx
+++ src/layouts/Admin/index.tsx
@@ -52,7 +52,7 @@ const App: React.FC = () => {
             <Content className={styles.content}>
               <Outlet />
             </Content>
-            <Footer className={styles.footer}>4566</Footer>
+            <Footer className={styles.footer}></Footer>
           </Layout>
 
           <Sider
